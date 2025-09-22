233cba679e5d83a00a94bf2a676fdaa8f1f0d724
JUNG JI MIN <wlals04977@naver.com>
2025-09-22 14:19:39 +0900

    움직임 함수 생성

--- animation_viewer.py
+++ animation_viewer.py
@@ -1,8 +1,22 @@
 from pico2d import *
-
 open_canvas()
 
 character = load_image('animation.png')
 
+def work():
+    pass
+def run():
+    pass
+def jump():
+    pass
+def push():
+    pass
+
+
+while True:
+    work()
+    run()
+    jump()
+    push()
 
 close_canvas()
