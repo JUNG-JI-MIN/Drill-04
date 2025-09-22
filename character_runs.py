60c66e458f1accc134849badd9b6dd482a9100e8
JUNG JI MIN <wlals04977@naver.com>
2025-09-22 14:14:37 +0900

    .

--- character_runs.py
+++ character_runs.py
@@ -3,10 +3,14 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
+character = load_image('character.png')
 
-# fill here
-
-
+frame = 0
+for x in range(0, 800, 10):
+    close_canvas()
+    grass.draw(400,30)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
 close_canvas()
-
