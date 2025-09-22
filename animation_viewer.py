5599ae6a4b03c5f9ff5de323c0e7222cd04b2f33
JUNG JI MIN <wlals04977@naver.com>
2025-09-22 14:42:01 +0900

    jump 함수 구현, 이동 및 애니메이션 효과

--- animation_viewer.py
+++ animation_viewer.py
@@ -1,4 +1,6 @@
 from pico2d import *
+def y_cange(y):
+    return 498 - y
 open_canvas()
 
 character = load_image('animation.png')
@@ -8,15 +10,30 @@ def work():
 def run():
     pass
 def jump():
+    for i in range(5):
+        x = 100
+        for frame in range(6):
+            clear_canvas()
+            character.clip_draw(20+frame * 80, y_cange(385), 60,60, x, 300,200,200)
+            x += 100
+            update_canvas()
+            delay(0.1)
+    delay(1.0)
     pass
-def push():
+def hit():
+    for i in range(5):
+        for frame in range(3):
+            clear_canvas()
+            character.clip_draw(20+frame * 80, y_cange(465), 60,60, 400, 300,200,200)
+            update_canvas()
+            delay(0.1)
+    delay(1.0)
     pass
-
 
 while True:
     work()
     run()
     jump()
-    push()
+    hit()
 
 close_canvas()
